0cd5598a51d3300d5cd460599818aaf468a3a635
Gordon Yoon <gordonyoon@gmail.com>
2015-08-26 15:07:23 -0700

    Tell Joke button uses a joke retrieved from javajokes

--- javajokes/src/main/java/com/example/Joker.java
+++ javajokes/src/main/java/com/example/Joker.java
@@ -1,4 +1,7 @@
 package com.example;
 
 public class Joker {
+    public String getJoke() {
+        return "Every wife tells her husband that he doesn't listen. To this day, I haven't heard it from mine.";
+    }
 }
