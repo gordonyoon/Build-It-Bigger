254aa112012d686d49cf3b722a6a506ff396e538
Gordon Yoon <gordonyoon@gmail.com>
2015-08-26 16:23:39 -0700

    Create jokedisplay library

--- javajokes/src/main/java/com/example/Joker.java
+++ javajokes/src/main/java/com/example/Joker.java
@@ -2,6 +2,6 @@ package com.example;
 
 public class Joker {
     public String getJoke() {
-        return "Every wife tells her husband that he doesn't listen. To this day, I haven't heard it from mine.";
+        return "Every wife tells her husband that he doesn't listen. To this day, I still haven't heard it from mine.";
     }
 }
