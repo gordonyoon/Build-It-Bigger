4d79be8cc77ce718e6acd1305ce2a7e021c23b98
Gordon Yoon <gordonyoon@gmail.com>
2015-08-27 17:47:17 -0700

    backend module supplies joke from javajokes

--- app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
+++ app/src/main/java/com/udacity/gradle/builditbigger/MainActivity.java
@@ -9,6 +9,7 @@ import android.view.View;
 import android.widget.Toast;
 
 import com.example.gordonyoon.builditbigger.backend.myApi.MyApi;
+import com.example.gordonyoon.jokedisplayer.JokeDisplayer;
 import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.extensions.android.json.AndroidJsonFactory;
 import com.google.api.client.googleapis.services.AbstractGoogleClientRequest;
@@ -24,7 +25,6 @@ public class MainActivity extends ActionBarActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
-
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -49,22 +49,19 @@ public class MainActivity extends ActionBarActivity {
     }
 
     public void tellJoke(View view) {
-//        Joker joker = new Joker();
-//        JokeDisplayer.start(this, joker.getJoke());
-
-        new EndpointsAsyncTask().execute("gordon");
+        new EndpointsAsyncTask().execute();
     }
 
-    private void showToast(String result) {
-        Toast.makeText(this, result, Toast.LENGTH_SHORT).show();
+    private void startJokeDisplayer(String joke) {
+        JokeDisplayer.start(this, joke);
     }
 
-    class EndpointsAsyncTask extends AsyncTask<String, Void, String> {
+    class EndpointsAsyncTask extends AsyncTask<Void, Void, String> {
         private final static String IP_GENYMOTION = "http://10.0.3.2:8080/_ah/api/";
         private final static String IP_ANDROID_EMULATOR = "http://10.0.2.2:8080/_ah/api/";
 
         @Override
-        protected String doInBackground(String... params) {
+        protected String doInBackground(Void... params) {
             MyApi.Builder builder = new MyApi.Builder(AndroidHttp.newCompatibleTransport(),
                     new AndroidJsonFactory(), null)
                     // options for running against local devappserver
@@ -80,17 +77,16 @@ public class MainActivity extends ActionBarActivity {
             // end options for devappserver
             MyApi myApiService = builder.build();
 
-            String name = params[0];
             try {
-                return myApiService.sayHi(name).execute().getData();
+                return myApiService.getJoke().execute().getData();
             } catch (IOException e) {
                 return e.getMessage();
             }
         }
 
         @Override
-        protected void onPostExecute(String result) {
-            showToast(result);
+        protected void onPostExecute(String joke) {
+            startJokeDisplayer(joke);
         }
     }
 }
--- backend/src/main/java/com/example/gordonyoon/builditbigger/backend/MyEndpoint.java
+++ backend/src/main/java/com/example/gordonyoon/builditbigger/backend/MyEndpoint.java
@@ -6,6 +6,7 @@
 
 package com.example.gordonyoon.builditbigger.backend;
 
+import com.example.Joker;
 import com.google.api.server.spi.config.Api;
 import com.google.api.server.spi.config.ApiMethod;
 import com.google.api.server.spi.config.ApiNamespace;
@@ -29,12 +30,20 @@ public class MyEndpoint {
     /**
      * A simple endpoint method that takes a name and says Hi back
      */
-    @ApiMethod(name = "sayHi")
-    public MyBean sayHi(@Named("name") String name) {
-        MyBean response = new MyBean();
-        response.setData("Hi, " + name);
+//    @ApiMethod(name = "sayHi")
+//    public MyBean sayHi(@Named("name") String name) {
+//        MyBean response = new MyBean();
+//        response.setData("Hi, " + name);
+//
+//        return response;
+//    }
 
-        return response;
+    @ApiMethod(name = "getJoke")
+    public MyBean getJoke() {
+        Joker joker = new Joker();
+        MyBean joke = new MyBean();
+        joke.setData(joker.getJoke());
+        return joke;
     }
 }
 
