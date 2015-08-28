7c9016d34f10dca1fec7b8e5dc19b1287388a18c
Gordon Yoon <gordonyoon@gmail.com>
2015-08-28 12:46:03 -0700

    Add: test for AsyncJokeDisplayer

--- app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
+++ app/src/androidTest/java/com/udacity/gradle/builditbigger/AsyncTaskTest.java
@@ -1,15 +1,40 @@
 package com.udacity.gradle.builditbigger;
 
-import android.test.AndroidTestCase;
+import android.test.UiThreadTest;
+
+import junit.framework.TestCase;
 
 import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
 
-import static com.udacity.gradle.builditbigger.MainActivity.*;
-
-
-public class AsyncTaskTest extends AndroidTestCase {
+import interfaces.EndpointResultListener;
 
 
+public class AsyncTaskTest extends TestCase implements EndpointResultListener {
+    AsyncJokeDisplayer asyncJokeDisplayer;
+    CountDownLatch signal;
+    String joke;
+
+    @Override
+    protected void setUp() throws Exception {
+        super.setUp();
+        asyncJokeDisplayer = new AsyncJokeDisplayer(this);
+        signal = new CountDownLatch(1);
+    }
+
+    @UiThreadTest
+    public void testRetrieveJoke() throws InterruptedException {
+        asyncJokeDisplayer.showJoke();
+        signal.await(30, TimeUnit.SECONDS);
+
+        assertTrue("The joke was not retrieved", !joke.isEmpty());
+    }
+
+    @Override
+    public void onJokeReceived(String joke) {
+        signal.countDown();
+        this.joke = joke;
+    }
 }
 
 
